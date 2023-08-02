e5e1c3166464b37d8ddd32cd00354276db7e02d9
ReallyAwesomeName <90869629+ReallyAwesomeName@users.noreply.github.com>
2023-08-02 00:23:11 -0400

    initial

---
+++ server/main.py
@@ -0,0 +1,26 @@
+#
+# main.py
+# Driver code for backend
+#
+# Author: Rin | Discord: Rin#0304
+# https://github.com/ReallyAwesomeName/kf2-tools
+#
+# =========================================================================== #
+#     This program provides tools to calculate things for Killing Floor 2     #
+#     Copyright (C) 2023  Rin                                                 #
+#                                                                             #
+#     This file is part of kf2-tools                                          #
+#                                                                             #
+#     This program is free software: you can redistribute it and/or modify    #
+#     it under the terms of the GNU General Public License as published by    #
+#     the Free Software Foundation, either version 3 of the License, or       #
+#     (at your option) any later version.                                     #
+#                                                                             #
+#     This program is distributed in the hope that it will be useful,         #
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of          #
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           #
+#     GNU General Public License for more details.                            #
+#                                                                             #
+#     You should have received a copy of the GNU General Public License       #
+#     along with this program.  If not, see <https://www.gnu.org/licenses/>.  #
+# =========================================================================== #
